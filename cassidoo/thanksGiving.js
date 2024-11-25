542af54ce02680d9101d65f37c421b30e9371302
Steve A <stevekalex@gmail.com>
2024-11-25 18:58:31 +0000

    first commit

---
+++ cassidoo/thanksGiving.js
@@ -0,0 +1,14 @@
+/*
+You're organizing a family meal. Given a list of dishes and their respective preparation times,
+return the minimum number of hours required to prepare all dishes if you can cook up to two dishes simultaneously.
+If a dish takes longer than the remaining time of the current hour, it will be moved to the next hour.
+
+mealPrep([120])
+2 // one single long dish
+
+mealPrep([30, 30, 30, 20])
+2 // multiple shorter dishes
+
+mealPrep([30, 25, 45, 30, 60, 15])
+3 // many dishes with varying times
+*/
